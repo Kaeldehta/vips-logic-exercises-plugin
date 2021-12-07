bd4dcad344af6a4bbf7db1d561dfedec6514fd97
Kaeldehta <floxx33@gmail.com>
2021-12-07 11:35:27 +0100

    renamed semantic_consequence to consequence

--- src/components/Semantic Tree/edit.tsx
+++ src/components/Semantic Tree/edit.tsx
@@ -3,14 +3,14 @@ import {useState} from "react";
 import FormulaInput from "./FormulaInput";
 
 interface Props {
-    semantic_consequence: string,
+    consequence: string,
     statements: string[],
 }
 
-const EditComponent = ({semantic_consequence, statements}: Props) => {
+const EditComponent = ({consequence, statements}: Props) => {
 
     return <>
-        <FormulaInput initial_formula={semantic_consequence} name="sem_cons"/>
+        <FormulaInput initial_formula={consequence} name="sem_cons"/>
     </>
 }
 
