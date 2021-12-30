95e95ca296d23849bb7710c7ee47616cf806d598
Kaeldehta <floxx33@gmail.com>
2021-12-30 17:44:47 +0100

    complete syntactic proof refactor

--- src/components/fc_exercise/Border.tsx
+++ src/components/fc_exercise/Border.tsx
@@ -3,5 +3,11 @@ interface Props {
     addBottom?: boolean
 }
 
-export default ({addBottom}: Props) =>
-    <div className={"w-4 border-l-2 border-l-black" + (addBottom ? " border-b-2 border-b-black" : "")}/> 
+export default ({addBottom}: Props) => {
+    if(addBottom) {
+        return <div className="flex self-end h-8 w-4 border-l-2 border-l-black border-b-2 border-b-black"/>
+    }
+    
+    return <div className="w-4 border-l-2 border-l-black"/> 
+}
+    
