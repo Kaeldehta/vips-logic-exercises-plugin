178438982b32db9100cfadc58c724edbb4247b1a
Kaeldehta <floxx33@gmail.com>
2022-01-14 11:12:51 +0100

    changed to ref manipulation

--- fc_exercise.php
+++ fc_exercise.php
@@ -6,28 +6,28 @@ class fc_exercise extends LogicExercise
     public function responseFromRequest($request)
     {
         $lines = $request["lines"];
-        foreach ($lines as $key => $value) {
-            if(isset($lines[$key]["line"]["from"]["line1"])) {
-                $lines[$key]["line"]["from"]["line1"] = intval($value["line"]["from"]["line1"]);
+        foreach ($lines as &$value) {
+            if(isset($value["line"]["from"]["line1"])) {
+                $value["line"]["from"]["line1"] = intval($value["line"]["from"]["line1"]);
             }
 
-            if(isset($lines[$key]["line"]["from"]["line2"])) {
-                $lines[$key]["line"]["from"]["line2"] = intval($value["line"]["from"]["line2"]);
+            if(isset($value["line"]["from"]["line2"])) {
+                $value["line"]["from"]["line2"] = intval($value["line"]["from"]["line2"]);
             }
 
-            if(isset($lines[$key]["line"]["from"]["line"])) {
-                $lines[$key]["line"]["from"]["line"] = intval($value["line"]["from"]["line"]);
+            if(isset($value["line"]["from"]["line"])) {
+                $value["line"]["from"]["line"] = intval($value["line"]["from"]["line"]);
             }
 
-            if(isset($lines[$key]["line"]["from"]["assumption1"])) {
-                $lines[$key]["line"]["from"]["assumption1"] = intval($value["line"]["from"]["assumption1"]);
+            if(isset($value["line"]["from"]["assumption1"])) {
+                $value["line"]["from"]["assumption1"] = intval($value["line"]["from"]["assumption1"]);
             }
 
-            if(isset($lines[$key]["line"]["from"]["assumption2"])) {
-                $lines[$key]["line"]["from"]["assumption2"] = intval($value["line"]["from"]["assumption2"]);
+            if(isset($value["line"]["from"]["assumption2"])) {
+                $value["line"]["from"]["assumption2"] = intval($value["line"]["from"]["assumption2"]);
             }
 
-            $lines[$key]["indentationLevel"] = intval($value["indentationLevel"]);
+            $value["indentationLevel"] = intval($value["indentationLevel"]);
         }
 
         return ["lines" => $lines];
