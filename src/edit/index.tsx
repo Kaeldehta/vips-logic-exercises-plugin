7d1edc5f9643e5039db1ba5d68ab0906867e22a8
Kaeldehta <floxx33@gmail.com>
2022-03-13 13:31:44 +0100

    refactor(edit): removed comment

--- src/edit/index.tsx
+++ src/edit/index.tsx
@@ -14,10 +14,6 @@ const Edit= ({separator}: Props) => {
         <div className="flex flex-row items-center gap-1">
         <Submit/>
         <Statements/>
-        {/* <button className="border-black border-2 px-3 hover:bg-gray-100" onClick={(event)=> {
-            event.preventDefault();
-            addNewStatement();
-        }}>+</button> */}
         {separator}
         <Formula allowPred selector={state => state.answer.consequence} actionCreator={setConsequence}/>
         </div>
