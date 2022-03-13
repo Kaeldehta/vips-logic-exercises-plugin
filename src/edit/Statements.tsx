31e33c40ba27503632ffcb5532bd58540ca8ffc1
Kaeldehta <floxx33@gmail.com>
2022-03-13 13:24:08 +0100

    feat(edit): better edit component
    allow addition and removal of statements

--- src/edit/Statements.tsx
+++ src/edit/Statements.tsx
@@ -1,13 +1,20 @@
+import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
+import DispatchActionButton from "../components/DispatchActionButton";
 import Formula from "../components/Formula";
 import { useTypedSelector } from "../hooks";
-import { setStatement } from "../redux/answer";
+import { addStatement, removeStatement, setStatement } from "../redux/answer";
 
 const Statements = () => {
 
     const statements = useTypedSelector(state => state.answer.statements.ids);
 
     return <>
-        {statements.map((id) => <Formula allowPred selector={state => state.answer.statements[id].formula} actionCreator={(statement: string) => setStatement({id, statement})}/>)}
+        {statements.map((id) => 
+        <div key={id} className="flex items-center">
+            <Formula allowPred selector={state => state.answer.statements.entries[id]} actionCreator={(statement: string) => setStatement({id, statement})}/>
+            <DispatchActionButton show action={removeStatement(id)} icon={FiMinusCircle}/>
+        </div>)}
+        <DispatchActionButton show icon={FiPlusCircle} action={addStatement()}/>
     </>
 }
 
