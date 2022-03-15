fdc5217070fd5ac3bd9723f35c83d978aab0738a
Kaeldehta <floxx33@gmail.com>
2022-03-15 11:01:28 +0100

    feat(from): better options
    get better options for semantic tree

--- src/components/From.tsx
+++ src/components/From.tsx
@@ -3,8 +3,14 @@ import { useDispatch } from "react-redux";
 import { useTypedSelector } from "../hooks";
 import { setFrom } from "../redux/response";
 import { LineId } from "../types";
+import { findParent } from "../utils";
 
-const FromSelect = ({id, index}: {id: LineId, index: number}) => {
+interface FromSelectProps {
+    id: LineId
+    index: number
+}
+
+const FromSelect = ({id, index}: FromSelectProps) => {
 
     const dispatch = useDispatch();
 
@@ -18,8 +24,31 @@ const FromSelect = ({id, index}: {id: LineId, index: number}) => {
     });
 
     const options = useTypedSelector(state => {
-        const index = state.response.present.ids.indexOf(id);
+        if(state.response.present.root) {
+            const options = [];
 
+            let next = id;
+
+            while(true) {
+                
+                const parentId = findParent(next, state.response.present);
+
+                if(!parentId) break;
+
+                options.push({
+                    label: state.response.present.ids.indexOf(parentId) + 1,
+                    value: parentId
+                });
+
+                next = parentId;
+            }
+
+            options.reverse()
+            
+            return options;
+        }
+        const index = state.response.present.ids.indexOf(id);
+    
         return state.response.present.ids.slice(0, index).map((id, index) => ({
             value: id,
             label: index + 1,
