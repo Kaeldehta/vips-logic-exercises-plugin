4dcf86cc71b28d8f850d257738a1abb93b15ce0e
Kaeldehta <floxx33@gmail.com>
2022-06-07 15:44:07 +0200

    fix(build.ts): fix missing files in zip file

--- build.ts
+++ build.ts
@@ -1,11 +1,9 @@
-import {replaceInFile} from "replace-in-file"
-import {copy, readFile, createWriteStream} from "fs-extra"
-import * as archiver from "archiver"
+import { replaceInFile } from "replace-in-file";
+import { copy, readFile, createWriteStream } from "fs-extra";
+import * as archiver from "archiver";
 
 const run = async () => {
-
-  await copy("templates/index.php", "dist/templates/index.php")
-  await copy("plugin.manifest", "dist/plugin.manifest")
+  await copy("templates/index.php", "dist/templates/index.php");
 
   const result = await readFile("templates/prod.php");
 
@@ -13,17 +11,20 @@ const run = async () => {
     files: ["dist/templates/index.php"],
     from: /<!--SCRIPT-->.*<!--END_SCRIPT-->/s,
     to: result.toString(),
-  })
+  });
 
-  const output = createWriteStream("package.zip")
+  const output = createWriteStream("package.zip");
 
-  const archive = archiver("zip")
+  const archive = archiver("zip");
 
-  archive.pipe(output)
+  archive.pipe(output);
 
-  archive.directory("dist/", false)
+  archive.directory("dist/", false);
+  archive.file("plugin.manifest", { name: "plugin.manifest" });
+  archive.file("logic_exercise.php", { name: "logic_exercise.php" });
+  archive.file("LogicExercises.php", { name: "LogicExercises.php" });
 
-  await archive.finalize()
-}
+  await archive.finalize();
+};
 
-run()
+run();
